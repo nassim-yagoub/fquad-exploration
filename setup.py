a2562ea75d22ee249b43f415130dbb414817401a
Nassim Yagoub <nass.yagoub50@gmail.com>
2020-11-29 01:18:01 +0100

    🎨 create setup.py and update requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='fquad-exploration',
+      version='1.0',
+      py_modules=[
+        'numpy'
+        'pandas'
+        'matplotlib'
+        'tqdm'
+        'nltk'
+        'sklearn'
+        'torch'
+        'transformers'
+        ],
+      )
